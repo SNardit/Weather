c27e96be5e303d41ef82eee4ce24564d7ec2f932
Svetlana Nardit <s.nardit@gmail.com>
2020-07-28 03:18:31 -0400

    Lesson1

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        setOnBtnChangeCity();
     }
+
+
+
+    private void initViews() {
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textViewCity);
+
+    }
+
+    private void setOnBtnChangeCity() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(R.string.new_york);
+            }
+        });
+    }
+
 }
