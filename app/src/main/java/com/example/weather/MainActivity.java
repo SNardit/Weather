c6109b594bb94737c23ae92bd0084a3176bf49f2
Svetlana Nardit <s.nardit@gmail.com>
2020-08-03 02:59:09 -0400

    Lesson3

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.weather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private TextView textView;
+    private Button cityChangeButton;
+    private TextView cityTextView;
+    private String TAG = "Life cycle";
+    private final String cityDataKey = "cityDataKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +22,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
         setOnBtnChangeCity();
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreate");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onSaveInstanceState");
+        String text = cityTextView.getText().toString();
+        outState.putString(cityDataKey, text);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Toast.makeText(getApplicationContext(), "onRestoreInstanceState", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onRestoreInstanceState");
+        cityTextView.setText(savedInstanceState.getString(cityDataKey));
+        super.onRestoreInstanceState(savedInstanceState);
+    }
 
     private void initViews() {
-        button = findViewById(R.id.button);
-        textView = findViewById(R.id.textViewCity);
-
+        cityChangeButton = findViewById(R.id.button);
+        cityTextView = findViewById(R.id.textViewCity);
     }
 
     private void setOnBtnChangeCity() {
-        button.setOnClickListener(new View.OnClickListener() {
+        cityChangeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText(R.string.new_york);
+                String currentCity = cityTextView.getText().toString();
+                String newYorkCity = getResources().getString(R.string.new_york);
+                if (currentCity.equals(newYorkCity)) {
+                    cityTextView.setText(R.string.moscow);
+                }
+                else cityTextView.setText(R.string.new_york);
             }
         });
     }
